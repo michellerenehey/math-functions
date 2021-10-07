19192e9f6cb6edc312a8c386f2cd1ca3171ff191
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-07 14:38:17 -0700

    successfully updated multiplyArrayWithThreeNumbers function

--- src/math-functions.js
+++ src/math-functions.js
@@ -111,7 +111,9 @@ you may continue to use the + operator for string concatenation.
 */
 
 export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
-
+    const multArrMult = multiply(multiply(multArr[0], multArr[1])[0], multArr[2])[0];
+    const multArrStr = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of 24.`;
+    return [multArrMult, multArrStr];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
